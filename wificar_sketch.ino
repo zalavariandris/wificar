705d8f41ea0befd2902583ff76f5c332c763b289
zalavariandris <zalavariandris@gmail.com>
2020-12-20 16:47:33 +0100

    strip down linked files

--- wificar_sketch.ino
+++ wificar_sketch.ino
@@ -1,30 +1,90 @@
 #include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+#include <ESP8266WebServer.h>
 #include <FS.h>
 
 int MAX_SPEED = 1023;
 
-/* Utility function */
-static inline int8_t sgn(int val) {
- if (val < 0) return -1;
- if (val==0) return 0;
- return 1;
+// Create webserver object on port 80
+ESP8266WebServer server(80);
+
+/* RC CAR */
+void SetEngines(int left, int right){
+  /*handling values from -1023 to +1023*/
+  Serial.printf("SetEngines: [%d - %d]\n", left, right);
+  // left engine
+  analogWrite(D1, abs(left));
+  digitalWrite(D3, left < 0 ? LOW : HIGH);
+
+  // right engine
+  analogWrite(D2, abs(right));
+  digitalWrite(D4, right < 0 ? LOW : HIGH);
 }
 
-/* networking*/
-// network credentials
-const char* ssid = "wifiitthon-2.4GHz";
-const char* password = "Grimp465M";
-
-// Create webserver object on port 80
-AsyncWebServer server(80);
-
+void MoveForwardLeft(){
+  //left
+  digitalWrite(D3, HIGH);
+  analogWrite(D2, MAX_SPEED/2);
+  
+  //right
+  digitalWrite(D3, HIGH);
+  analogWrite(D4, MAX_SPEED);
+}
 
 void MoveForward(){
+  Serial.println("Car->MoveForward");
   //left
   digitalWrite(D3, LOW);
   analogWrite(D1, MAX_SPEED);
+  
+  //right
+  digitalWrite(D4, LOW);
+  analogWrite(D2, MAX_SPEED);
+}
+
+void MoveForwardRight(){
+    //left
+  digitalWrite(D3, HIGH);
+  analogWrite(D2, MAX_SPEED);
+  
+  //right
+  digitalWrite(D3, HIGH);
+  analogWrite(D4, MAX_SPEED/2);
+}
+
+void RotateLeft(){
+  //left
+  digitalWrite(D3, LOW);
+  analogWrite(D1, MAX_SPEED);
+
+  // right
+  digitalWrite(D4, HIGH);
+  analogWrite(D2, MAX_SPEED);
+}
+
+void MoveStop(){
+  //left
+  digitalWrite(D3, LOW);
+  analogWrite(D1, 0);
+
+  // right
+  digitalWrite(D4, LOW);
+  analogWrite(D2, 0);
+}
+
+void RotateRight(){
+  //left
+  digitalWrite(D3, HIGH);
+  analogWrite(D1, MAX_SPEED);
+
+  // right
+  digitalWrite(D4, LOW);
+  analogWrite(D2, MAX_SPEED);
+}
+
+void MoveBackwardLeft(){
+  //left
+  digitalWrite(D3, LOW);
+  analogWrite(D1, MAX_SPEED/2);
   
   //right
   digitalWrite(D4, LOW);
@@ -41,64 +101,69 @@ void MoveBackward(){
   analogWrite(D2, MAX_SPEED);
 }
 
-void MoveStop(){
+void MoveBackwardRight(){
   //left
   digitalWrite(D3, LOW);
-  analogWrite(D1, 0);
-
-  // right
-  digitalWrite(D4, LOW);
-  analogWrite(D2, 0);
-}
-
-void RotateRight(){
-    //left
-  digitalWrite(D3, HIGH);
   analogWrite(D1, MAX_SPEED);
-
-  // right
-  digitalWrite(D4, HIGH);
-  analogWrite(D2, 0);
+  
+  //right
+  digitalWrite(D4, LOW);
+  analogWrite(D2, MAX_SPEED/2);
 }
 
-void RotateLeft(){
-    //left
-  digitalWrite(D3, HIGH);
-  analogWrite(D1, 0);
-
-  // right
-  digitalWrite(D4, HIGH);
-  analogWrite(D2, MAX_SPEED);
-}
-
-void SetEngines(int left, int right){
-  // left engine
-  analogWrite(D1, abs(left));
-  digitalWrite(D3, left < 0 ? LOW : HIGH);
-
-  // right engine
-  analogWrite(D2, abs(right));
-  digitalWrite(D4, right < 0 ? LOW : HIGH);
+bool HandleCommand(String command){
+  Serial.print("handleCommand: ");
+  Serial.println(command);
+  if(command=="MoveStop"){
+    MoveStop();
+    return true;
+  }else if(command=="MoveForward"){
+    MoveForward();
+    return true;
+  }if(command=="MoveForwardLeft"){
+    MoveForwardLeft();
+    return true;
+  }if(command=="MoveForwardRight"){
+    MoveForwardRight();
+    return true;
+  }if(command=="RotateLeft"){
+    RotateLeft();
+    return true;
+  }if(command=="RotateRight"){
+    RotateRight();
+    return true;
+  }if(command=="MoveBackwardLeft"){
+    MoveBackwardLeft();
+    return true;
+  }if(command=="MoveBackward"){
+    MoveBackward();
+    return true;
+  }if(command=="MoveBackwardRight"){
+    MoveBackwardRight();
+    return true;
+  }else{
+    return false;
+  }
 }
 
 void setup() {
+  /*Logging*/
+  Serial.begin(9600);
+  delay(10);
+
+  /* CAR */
+  // setup pins for output
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
-
   MoveStop();
-  
-  Serial.begin(115200);
-  delay(10);
 
-  // Initialize SPIFFS
-  if(!SPIFFS.begin()){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-
+  /* WIFI */
   // Connect to WiFi network
+  // network credentials
+  const char* ssid = "wifiitthon-2.4GHz";
+  const char* password = "Grimp465M";
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
@@ -111,50 +176,64 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
 
-
-  // Start the server
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-  server.on("/vue.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/vue.js", "text/javascript");
-  });
-  
-  server.on("/command", HTTP_POST, [](AsyncWebServerRequest *request){
-    Serial.println("handle command:");
-    int params = request->params();
-    AsyncWebParameter* cmdParam = request->getParam("cmd");
-    String command = cmdParam->value();
-
-    if(command == "MoveStop"){MoveStop();}
-    else if (command == "MoveForward" ){MoveForward(); }
-    else if (command == "MoveBackward"){MoveBackward();}
-    request->send(200);
-  });
-  
-  server.on("/engines", HTTP_POST, [](AsyncWebServerRequest *request){
-    Serial.println("Handle engines:");
-    AsyncWebParameter* leftEngineParam = request->getParam("left");
-    AsyncWebParameter* rightEngineParam = request->getParam("right");
-    Serial.printf(" left:%s right: %s\n", leftEngineParam->value().c_str(), rightEngineParam->value().c_str());
-    
-    // Set engines
-    SetEngines(leftEngineParam->value().toInt(), rightEngineParam->value().toInt()); 
-    request->send(200);
-  });
-  server.begin();
-  Serial.println("Server started");
-
   // Print the IP address
   Serial.print("Use this URL to connect: ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
+
+  /* FILE SYSTEM*/
+  // Initialize SPIFFS
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  /* SERVER */
+  
+
+  server.on("/command", [](){
+    String cmd = server.arg("cmd");
+    Serial.printf("received command: %s\n", cmd.c_str());
+    if( HandleCommand(cmd) ){
+      server.send(200, "OK", "text/plain");
+    }else{
+      server.send(400, "No such command", "text/plain");
+    }
+  });
+  
+  server.on("/engines", [](){
+    Serial.print("received engines");
+    int left = server.arg("left").toInt();
+    int right = server.arg("right").toInt();
+    Serial.printf("left: %d, right: %d\n", left, right);
+    SetEngines(left, right);
+    server.send(200, "OK", "text/plain");
+  });
+
+  server.serveStatic("/", SPIFFS, "/", "max-age=86400");
+  
+//  
+//  server.on("/engines", HTTP_POST, [](AsyncWebServerRequest *request){
+//    Serial.println("Handle engines:");
+//    AsyncWebParameter* leftEngineParam = request->getParam("left");
+//    AsyncWebParameter* rightEngineParam = request->getParam("right");
+//    Serial.printf(" left:%s right: %s\n", leftEngineParam->value().c_str(), rightEngineParam->value().c_str());
+//    
+//    // Set engines
+//    SetEngines(leftEngineParam->value().toInt(), rightEngineParam->value().toInt()); 
+//    request->send(200);
+//  });
+  
+  server.onNotFound([](){
+    server.send(404, "text/plain", "404: Not Found");
+  });
+
+  server.begin();
+  Serial.println("HTTP Server started!");
 }
 
+
 void loop() {
-  
+  server.handleClient();
 }
