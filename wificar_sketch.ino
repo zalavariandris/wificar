18e244e8e7f85a9bba7bef176424fe1ac63bb994
zalavariandris <zalavariandris@gmail.com>
2020-12-21 15:05:43 +0100

    working

--- wificar_sketch.ino
+++ wificar_sketch.ino
@@ -5,147 +5,177 @@
 
 int MAX_SPEED = 1023;
 
+// network credentials
+const char* ssid = "wifiitthon-2.4GHz";
+const char* password = "Grimp465M";
+// port default is 80
+const int port=80;
+
 // Create webserver object on port 80
-ESP8266WebServer server(80);
+ESP8266WebServer server(port);
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 184);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
 
-/* RC CAR */
-void SetEngines(int left, int right){
-  /*handling values from -1023 to +1023*/
-  Serial.printf("SetEngines: [%d - %d]\n", left, right);
-  // left engine
-  analogWrite(D1, abs(left));
-  digitalWrite(D3, left < 0 ? LOW : HIGH);
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8); // optional
+IPAddress secondaryDNS(8, 8, 4, 4); // optional
 
-  // right engine
-  analogWrite(D2, abs(right));
-  digitalWrite(D4, right < 0 ? LOW : HIGH);
-}
-
-void MoveForwardLeft(){
-  //left
-  digitalWrite(D3, HIGH);
-  analogWrite(D2, MAX_SPEED/2);
-  
-  //right
-  digitalWrite(D3, HIGH);
-  analogWrite(D4, MAX_SPEED);
-}
-
-void MoveForward(){
-  Serial.println("Car->MoveForward");
-  //left
-  digitalWrite(D3, LOW);
-  analogWrite(D1, MAX_SPEED);
-  
-  //right
-  digitalWrite(D4, LOW);
-  analogWrite(D2, MAX_SPEED);
-}
-
-void MoveForwardRight(){
-    //left
-  digitalWrite(D3, HIGH);
-  analogWrite(D2, MAX_SPEED);
-  
-  //right
-  digitalWrite(D3, HIGH);
-  analogWrite(D4, MAX_SPEED/2);
-}
-
-void RotateLeft(){
-  //left
-  digitalWrite(D3, LOW);
-  analogWrite(D1, MAX_SPEED);
-
-  // right
-  digitalWrite(D4, HIGH);
-  analogWrite(D2, MAX_SPEED);
-}
-
-void MoveStop(){
-  //left
-  digitalWrite(D3, LOW);
-  analogWrite(D1, 0);
-
-  // right
-  digitalWrite(D4, LOW);
-  analogWrite(D2, 0);
-}
-
-void RotateRight(){
-  //left
-  digitalWrite(D3, HIGH);
-  analogWrite(D1, MAX_SPEED);
-
-  // right
-  digitalWrite(D4, LOW);
-  analogWrite(D2, MAX_SPEED);
-}
-
-void MoveBackwardLeft(){
-  //left
-  digitalWrite(D3, LOW);
-  analogWrite(D1, MAX_SPEED/2);
-  
-  //right
-  digitalWrite(D4, LOW);
-  analogWrite(D2, MAX_SPEED);
-}
-
-void MoveBackward(){
-  //left
-  digitalWrite(D3, HIGH);
-  analogWrite(D1, MAX_SPEED);
-  
-  //right
-  digitalWrite(D4, HIGH);
-  analogWrite(D2, MAX_SPEED);
-}
-
-void MoveBackwardRight(){
-  //left
-  digitalWrite(D3, LOW);
-  analogWrite(D1, MAX_SPEED);
-  
-  //right
-  digitalWrite(D4, LOW);
-  analogWrite(D2, MAX_SPEED/2);
-}
-
-bool HandleCommand(String command){
-  Serial.print("handleCommand: ");
-  Serial.println(command);
-  if(command=="MoveStop"){
-    MoveStop();
-    return true;
-  }else if(command=="MoveForward"){
-    MoveForward();
-    return true;
-  }if(command=="MoveForwardLeft"){
-    MoveForwardLeft();
-    return true;
-  }if(command=="MoveForwardRight"){
-    MoveForwardRight();
-    return true;
-  }if(command=="RotateLeft"){
-    RotateLeft();
-    return true;
-  }if(command=="RotateRight"){
-    RotateRight();
-    return true;
-  }if(command=="MoveBackwardLeft"){
-    MoveBackwardLeft();
-    return true;
-  }if(command=="MoveBackward"){
-    MoveBackward();
-    return true;
-  }if(command=="MoveBackwardRight"){
-    MoveBackwardRight();
-    return true;
-  }else{
-    return false;
+class Car{
+  public:
+  Car(){
   }
-}
+
+  void setup(){
+      pinMode(D1, OUTPUT);
+      pinMode(D2, OUTPUT);
+      pinMode(D3, OUTPUT);
+      pinMode(D4, OUTPUT);
+      MoveStop();
+  }
+
+  void SetEngines(int left, int right){
+    /*handling values from -1023 to +1023*/
+    Serial.printf("SetEngines: [%d - %d]\n", left, right);
+    // left engine
+    digitalWrite(D3, left < 0 ? LOW : HIGH);
+    analogWrite(D1, abs(left));
+  
+    // right engine
+    digitalWrite(D4, right < 0 ? LOW : HIGH);
+    analogWrite(D2, abs(right));
+    
+  }
+  
+  void MoveForwardLeft(){
+    //left
+    digitalWrite(D3, HIGH);
+    analogWrite(D1, MAX_SPEED/2);
+    
+    //right
+    digitalWrite(D3, HIGH);
+    analogWrite(D2, MAX_SPEED);
+  }
+  
+  void MoveForward(){
+    Serial.println("Car->MoveForward");
+    //left
+    digitalWrite(D3, LOW);
+    analogWrite(D1, MAX_SPEED);
+    
+    //right
+    digitalWrite(D4, LOW);
+    analogWrite(D2, MAX_SPEED);
+  }
+  
+  void MoveForwardRight(){
+      //left
+    digitalWrite(D3, HIGH);
+    analogWrite(D1, MAX_SPEED);
+    
+    //right
+    digitalWrite(D4, HIGH);
+    analogWrite(D2, MAX_SPEED/2);
+  }
+  
+  void RotateLeft(){
+    //left
+    digitalWrite(D3, LOW);
+    analogWrite(D1, MAX_SPEED);
+  
+    // right
+    digitalWrite(D4, HIGH);
+    analogWrite(D2, MAX_SPEED);
+  }
+  
+  void MoveStop(){
+    //left
+    digitalWrite(D3, LOW);
+    analogWrite(D1, 0);
+  
+    // right
+    digitalWrite(D4, LOW);
+    analogWrite(D2, 0);
+  }
+  
+  void RotateRight(){
+    //left
+    digitalWrite(D3, HIGH);
+    analogWrite(D1, MAX_SPEED);
+  
+    // right
+    digitalWrite(D4, LOW);
+    analogWrite(D2, MAX_SPEED);
+  }
+  
+  void MoveBackwardLeft(){
+    //left
+    digitalWrite(D3, LOW);
+    analogWrite(D1, MAX_SPEED/2);
+    
+    //right
+    digitalWrite(D4, LOW);
+    analogWrite(D2, MAX_SPEED);
+  }
+  
+  void MoveBackward(){
+    //left
+    digitalWrite(D3, HIGH);
+    analogWrite(D1, MAX_SPEED);
+    
+    //right
+    digitalWrite(D4, HIGH);
+    analogWrite(D2, MAX_SPEED);
+  }
+  
+  void MoveBackwardRight(){
+    //left
+    digitalWrite(D3, LOW);
+    analogWrite(D1, MAX_SPEED);
+    
+    //right
+    digitalWrite(D4, LOW);
+    analogWrite(D2, MAX_SPEED/2);
+  }
+  
+  bool HandleCommand(String command){
+    Serial.print("handleCommand: ");
+    Serial.println(command);
+    if(command=="MoveStop"){
+      MoveStop();
+      return true;
+    }else if(command=="MoveForward"){
+      MoveForward();
+      return true;
+    }if(command=="MoveForwardLeft"){
+      MoveForwardLeft();
+      return true;
+    }if(command=="MoveForwardRight"){
+      MoveForwardRight();
+      return true;
+    }if(command=="RotateLeft"){
+      RotateLeft();
+      return true;
+    }if(command=="RotateRight"){
+      RotateRight();
+      return true;
+    }if(command=="MoveBackwardLeft"){
+      MoveBackwardLeft();
+      return true;
+    }if(command=="MoveBackward"){
+      MoveBackward();
+      return true;
+    }if(command=="MoveBackwardRight"){
+      MoveBackwardRight();
+      return true;
+    }else{
+      return false;
+    }
+  }
+};
+
+Car car;
 
 void setup() {
   /*Logging*/
@@ -154,11 +184,7 @@ void setup() {
 
   /* CAR */
   // setup pins for output
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
-  MoveStop();
+  car.setup();
 
   /* WIFI */
   /* mDNS */
@@ -170,14 +196,16 @@ void setup() {
   Serial.println("mDNS name: wificar.local");
   
   /* Station Mode */
-  // Connect to WiFi network
-  // network credentials
-  const char* ssid = "wifiitthon-2.4GHz";
-  const char* password = "Grimp465M";
+  // fix static ip
+//  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+//    Serial.println("STA Failed to configure");
+//  }
+
   Serial.println("");
   Serial.print("Connecting to WiFi: ");
   Serial.print(ssid);
 
+  // Connect to WiFi network
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -186,6 +214,8 @@ void setup() {
   Serial.println("");
   Serial.print("WiFi connected, IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
 
   /* FILE SYSTEM*/
   // Initialize SPIFFS
@@ -198,7 +228,7 @@ void setup() {
   server.on("/command", [](){
     String cmd = server.arg("cmd");
     Serial.printf("received command: %s\n", cmd.c_str());
-    if( HandleCommand(cmd) ){
+    if( car.HandleCommand(cmd) ){
       server.send(200, "OK", "text/plain");
     }else{
       server.send(400, "No such command", "text/plain");
@@ -210,23 +240,11 @@ void setup() {
     int left = server.arg("left").toInt();
     int right = server.arg("right").toInt();
     Serial.printf("left: %d, right: %d\n", left, right);
-    SetEngines(left, right);
+    car.SetEngines(left, right);
     server.send(200, "OK", "text/plain");
   });
 
   server.serveStatic("/", SPIFFS, "/", "max-age=86400");
-  
-//  
-//  server.on("/engines", HTTP_POST, [](AsyncWebServerRequest *request){
-//    Serial.println("Handle engines:");
-//    AsyncWebParameter* leftEngineParam = request->getParam("left");
-//    AsyncWebParameter* rightEngineParam = request->getParam("right");
-//    Serial.printf(" left:%s right: %s\n", leftEngineParam->value().c_str(), rightEngineParam->value().c_str());
-//    
-//    // Set engines
-//    SetEngines(leftEngineParam->value().toInt(), rightEngineParam->value().toInt()); 
-//    request->send(200);
-//  });
   
   server.onNotFound([](){
     server.send(404, "text/plain", "404: Not Found");
